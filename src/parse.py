5fa340334c0b3fe387dba5105af8e371875d4c5e
Gaganpreet <gaganpreet.arora@gmail.com>
2013-09-05 13:50:05 +0530

    Fixes and features: * Added another H-1B pattern * Fixed regex * Crawl all pages with a delay of 30 seconds * Added duplicate handling * Accept CLI arguments

--- src/parse.py
+++ src/parse.py
@@ -3,6 +3,9 @@ import lxml.html
 import re
 import requests
 import json
+import time
+import sys
+import os
 from pygeocoder import Geocoder
 from pygeolib import GeocoderError
 from functools import lru_cache
@@ -29,7 +32,7 @@ def guess_type_of_position(text):
 
     # Check if any of h1b_patterns exist in text but isn't 
     # prefixed by the word 'no'
-    h1b_patterns = ['h1b', 'h1-b']
+    h1b_patterns = ['h1b', 'h1-b', 'h-1b']
     h1b = any(pattern in text_lower for pattern in h1b_patterns) and \
           not any('no ' + pattern in text_lower for pattern in h1b_patterns) 
 
@@ -71,7 +74,7 @@ def guess_location(text, aggressive=True):
     #       Two Words, AB
     # Will also pickup locations like:
     #       London, UK
-    match = re.search('(([A-Z][^ A-Z]{2,}){1,2}, [A-Z]{2})', text)
+    match = re.search(r'(([A-Z][^ A-Z]{2,} ?){1,2}, [A-Z]{2})\W', text)
     if match:
         return match.group(1)
 
@@ -80,7 +83,7 @@ def guess_location(text, aggressive=True):
         # The aggressive bool is necessary otherwise there'll
         # be a lot of false positives, by default this will only be
         # evaluated for the first line of the comment
-        match = re.search('([A-Z][^ ]{2,}, [A-Z][^ ]+)', text)
+        match = re.search('(([A-Z][^ A-Z,]{2,} ?){1,2}?, [A-Z][^ ]+)', text)
         if match:
             return match.group(1)
 
@@ -122,22 +125,25 @@ def get_comment_objects(html):
     for row in rows:
         if row.xpath(".//span[@class='comment']"):
             comment = row.xpath(".//span/font")[0]
+            user = row.xpath('.//span/a[1]')[0].text_content()
             url = row.xpath('.//span/a[2]')[0].attrib['href']
-            yield url, comment
+            yield url, user, comment
 
     more = html.xpath(".//a[text()='More']")
     if len(more) == 1:
-        yield more[0].attrib['href']
-        #for comment in get_comments(more[0].attrib['href']):
-        #   yield comment
+        time.sleep(30)
+        for comment in get_comment_objects(fetch_page('http://news.ycombinator.com' + more[0].attrib['href'])):
+            yield comment
 
 def fetch_page(url):
     '''
         GET a url and return the response if status code is 200
     '''
+    print('Fetching ' + url)
     response = requests.get(url)
+    print(response.status_code)
     if response.status_code == 200:
-        return response
+        return response.text
 
 @lru_cache(maxsize=200, typed=False)
 def geocode(location):
@@ -181,13 +187,20 @@ def extract_text(node):
             text += child + '\n'
     return text
 
-def parse_and_write(html, currrent_file, previous_month_data):
+def is_duplicate(key, store):
+    if store:
+        return not key in store
+    return True
+
+def parse_and_write(start_html, currrent_file, previous_month_users):
     '''
         Parse the html, organize into objects and write to `current_file'
         as json
     '''
     results = []
-    for url, comment in get_comment_objects(html):
+    for url, user, comment in get_comment_objects(start_html):
+        if not url:
+            continue
         comment_text = extract_text(comment)
         comment_html = lxml.html.tostring(comment, encoding='utf-8')
         comment_html = comment_html.decode('utf-8')
@@ -198,43 +211,51 @@ def parse_and_write(html, currrent_file, previous_month_data):
         remote, h1b, intern_ = guess_type_of_position(first_line)
 
         # Try guessing location on first line, then on the whole thing
+        location, lat, lon, formatted_address, country = [None]*5
         location = guess_location(first_line)
         if remote == False and location == None:
             location = guess_location(comment_text, False)
 
         if location:
             (lat, lon), formatted_address, country = geocode(location)
-            if not lat:
-                location = None
+            if not lat or len(formatted_address) > 50:
+                location, lat, lon, formatted_address, country = [None]*5
 
-        result = dict(remote=remote, location=location, h1b=h1b, 
+        result = dict(remote=remote, intern=intern_, h1b=h1b, 
                       short_desc=comment_text[:256], 
                       url=BASE_URL + url,
                       full_text=comment_text, full_html=comment_html,
-                      intern=intern_,
+                      user=user, freshness=is_duplicate(user, previous_month_users),
+                      location=location,
                       lat=lat, lon=lon, country=country,
                       address=formatted_address,)
         results.append(result)
+#    pprint(results)
     json.dump(results, open(currrent_file, 'w'), indent=4, sort_keys=True)
 
 
 def main():
-    html = open('html', encoding='utf8').read()
-    now = datetime.now()
-    previous_month = now - timedelta(days=30)
+    url = 'https://news.ycombinator.com/item?id=' + sys.argv[1]
+    year, month = sys.argv[2].split('-')
 
-    current_file = './data/%s.json' % now.strftime('%Y-%m')
+    date = datetime(year=int(year), month=int(month), day=1)
+    previous_month = date - timedelta(days=30)
+
+    current_file = './data/%s.json' % date.strftime('%Y-%m')
     previous_month_file = './data/%s.json' %  previous_month.strftime('%Y-%m')
 
+    previous_month_users = None
     try:
         previous_month_data = json.load(open(previous_month_file))
+        previous_month_users = [p['user'] for p in previous_month_data]
     except IOError:
         previous_month_data = False
 
-    parse_and_write(html, current_file, previous_month_data)
+    parse_and_write(fetch_page(url), current_file, previous_month_users)
+
+    available_data = json.dumps(os.listdir('./data'))
+    with open('data/months.js', 'w') as f:
+        f.write('var available_data = %s;'%(available_data));
     
-
-
-
 if __name__ == '__main__':
     main()
