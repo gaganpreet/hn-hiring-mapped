35bf701e0aca232b30dd9b7c54747937d1b9d122
Gaganpreet <gaganpreet.arora@gmail.com>
2013-09-18 21:50:34 +0530

    Code clean up and few lint fixes

--- src/parse.py
+++ src/parse.py
@@ -9,11 +9,9 @@ import os
 from pygeocoder import Geocoder
 from pygeolib import GeocoderError
 from functools import lru_cache
-from lxml import etree
 from datetime import datetime, timedelta
-from pprint import pprint
 
-BASE_URL='https://news.ycombinator.com/'
+BASE_URL = 'https://news.ycombinator.com/'
 
 def guess_type_of_position(text):
     '''
@@ -22,7 +20,7 @@ def guess_type_of_position(text):
         Types of position:
             * Remote
             * H1B (and variations)
-            * Intern 
+            * Intern
     '''
     text_lower = text.lower()
     is_remote = False
@@ -30,11 +28,11 @@ def guess_type_of_position(text):
         if 'no remote' not in text_lower:
             is_remote = True
 
-    # Check if any of h1b_patterns exist in text but isn't 
+    # Check if any of h1b_patterns exist in text but isn't
     # prefixed by the word 'no'
     h1b_patterns = ['h1b', 'h1-b', 'h-1b']
-    h1b = any(pattern in text_lower for pattern in h1b_patterns) and \
-          not any('no ' + pattern in text_lower for pattern in h1b_patterns) 
+    h1b = (any(pattern in text_lower for pattern in h1b_patterns) and
+            not any('no ' + pattern in text_lower for pattern in h1b_patterns))
 
     intern_ = False
     if re.search(r'\Wintern\W', text_lower):
@@ -51,17 +49,21 @@ def guess_location(text, aggressive=True):
             * If aggressive is true, look for patterns like City, State
     '''
     # Some commonly used locations and their oft used synonyms
-    common_locations = {'San Francisco': ['SF', 'San Francisco', 
-                                          'SoMa', 'SOMA', 'Bay Area',
+    common_locations = {'San Francisco': ['SF',
+                                          'San Francisco',
+                                          'SoMa',
+                                          'SOMA',
+                                          'Bay Area',
                                           'SAN FRANCISCO'],
-                        'Palo Alto': ['Palo Alto'],
-                        'Mountain View': ['Mountain View'],
-                        'New York': ['NYC', 'NY', 'New York'],
+                        'New York': ['NYC',
+                                     'NY',
+                                     'New York'],
                         'London': ['London'],
                         'Berlin': ['Berlin'],
+                        'Palo Alto': ['Palo Alto'],
+                        'Mountain View': ['Mountain View'],
                         }
 
-    
     # Look for a common location
     for location, names in common_locations.items():
         for name in names:
@@ -116,9 +118,9 @@ def get_comment_objects(html):
         Parse the page with lxml and get the top level comments
     '''
     html = lxml.html.fromstring(html)
-    
+
     # This is a complicated xpath because HN's html is terrible
-    # Another reason is that we need only the top level comments, 
+    # Another reason is that we need only the top level comments,
     # and ignore the children
     rows = html.xpath("//img[@width=0]/../..")
 
@@ -132,7 +134,8 @@ def get_comment_objects(html):
     more = html.xpath(".//a[text()='More']")
     if len(more) == 1:
         time.sleep(30)
-        for comment in get_comment_objects(fetch_page('http://news.ycombinator.com' + more[0].attrib['href'])):
+        page = fetch_page('http://news.ycombinator.com' + more[0].attrib['href'])
+        for comment in get_comment_objects(page):
             yield comment
 
 def fetch_page(url):
@@ -159,7 +162,7 @@ def geocode(location):
         return ((None, None), None, None)
     lat, lon = results.coordinates
     formatted_address = results.formatted_address
-    
+
     country = None
     for component in results.current_data['address_components']:
         if 'country' in component['types']:
@@ -227,15 +230,19 @@ def parse_and_write(start_html, currrent_file, previous_month_users):
 
         location, lat, lon, formatted_address, country = location_and_geocode(comment, first_line)
 
-        result = dict(remote=remote, intern=intern_, h1b=h1b, 
+        result = dict(remote=remote,
+                      intern=intern_,
+                      h1b=h1b,
                       url=(BASE_URL + url),
                       full_html=comment_html,
-                      user=user, freshness=is_not_duplicate(user, previous_month_users),
+                      user=user,
+                      freshness=is_not_duplicate(user, previous_month_users),
                       location=location,
-                      lat=lat, lon=lon, country=country,
+                      lat=lat,
+                      lon=lon,
+                      country=country,
                       address=formatted_address,)
         results.append(result)
-#    pprint(results)
     json.dump(results, open(currrent_file, 'w'), indent=4, sort_keys=True)
 
 
@@ -257,11 +264,13 @@ def main():
     parse_and_write(fetch_page(url), current_file, previous_month_users)
 
     # Dump to a file the months for which data is available, ignoring hidden files
-    available_data = [filename.split('.')[0] for filename in os.listdir('./web/data') if filename[0] is not '.']
+    available_data = [filename.split('.')[0]
+                        for filename in os.listdir('./web/data')
+                        if filename[0] is not '.']
     available_data.sort()
     available_data = json.dumps(available_data)
     with open('./web/js/months.js', 'w') as f:
         f.write('var available_data = %s;'%(available_data));
-    
+
 if __name__ == '__main__':
     main()
